ec40a553684498fdd0f1bf32e8c9932d1dc3dfdb
Aaron Kaczmarek <aaronmichaelmusic@gmail.com>
2019-02-13 06:13:19 -0500

    metricbeat setup

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php 
+
+echo "<html><body>";
+
+echo "<h1>hello world</h1>";
+
+echo "<h3>you've reached: " . $_SERVER['REQUEST_URI'] . "</h3>";
+
+// var_dump($_SERVER);
+
+echo "</body></html>";
+
+
+
